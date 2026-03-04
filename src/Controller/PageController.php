09d0f283f963d3690c774d1f690043efb2862ef6
Anthony L <laplane_anthony@yahoo.fr>
2026-03-04 16:28:12 +0100

    page d'accueil

---
+++ src/Controller/PageController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PageController extends AbstractController
+{
+    #[Route('/', name: 'app_page')]
+    public function index(): Response
+    {
+        $username =  "John";
+        $age = 20;
+        $movies = [
+            ["name" => "Inception", "year" => "2009"],
+            ["name" => "Matrix", "year" => "2003"],
+            ["name" => "Tenet", "year" => "2015"],
+        ];
+        return $this->render('page/index.html.twig', [
+            'username' => $username,
+            'age' => $age,
+            'movies' => $movies,
+        ]);
+    }
+}
