db80c89396fd1f811123cd68c6e50928d9f5fbe8
Anthony L <laplane_anthony@yahoo.fr>
2026-03-05 16:31:43 +0100

    movies

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,25 +10,24 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PageController extends AbstractController
 {
     #[Route('/', name: 'app_page')]
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
-        $username =  "John";
-        $age = 20;
-        $movies = [
-            ["name" => "Inception", "year" => "2009"],
-            ["name" => "Matrix", "year" => "2003"],
-            ["name" => "Tenet", "year" => "2015"],
-        ];
+        $movies = $movieRepository->findAll();
+
         return $this->render('page/index.html.twig', [
-            'username' => $username,
-            'age' => $age,
             'movies' => $movies,
         ]);
     }
 
-    #[Route('/about', name:'app_about')]
+    #[Route('/about', name: 'app_about')]
     public function about(): Response
     {
         return $this->render('page/about.html.twig');
+    }
+
+    #[Route('/legal', name: 'app_legal')]
+    public function legal(): Response
+    {
+        return $this->render('page/legal.html.twig');
     }
 }
---
+++ src/Entity/Movie.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $synopsis = null;
+
+    #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
+    private ?\DateTimeImmutable $releaseDate = null;
+
+    #[ORM\ManyToOne(inversedBy: 'movies')]
+    private ?Director $director = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(?string $synopsis): static
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTimeImmutable
+    {
+        return $this->releaseDate;
+    }
+
+    public function setReleaseDate(?\DateTimeImmutable $releaseDate): static
+    {
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    public function getDirector(): ?Director
+    {
+        return $this->director;
+    }
+
+    public function setDirector(?Director $director): static
+    {
+        $this->director = $director;
+
+        return $this;
+    }
+}
